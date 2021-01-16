39a6eb17748fa08cc2435e84f1fcb8ab57d94dd0
Vasilis-Moustis <34523341+Vasilis-Moustis@users.noreply.github.com>
2021-01-16 18:47:35 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,37 @@
 #########################################################################
+#########################  I M P O R T S ################################
+# you gonna need to install sys from pip3
+
+import itertools
+import random
+import array
+import time
+import os
+
+#########################################################################
+#######################  N O T   I N   U S E ############################
+def checkPotentialPoints(playStyle, bonusPoints, myDeck, myTable):
+    if bonusPoints:
+        return checkSimilarValues(playStyle, bonusPoints, myDeck, myTable) and checkSimilarSymbols(playStyle, bonusPoints, myDeck, myTable)
+    else:
+        return checkSimilarValues(playStyle, bonusPoints, myDeck, myTable)
+
+def checkSimilarValues(playStyle, bonusPoints, myDeck, myTable):
+    for i in len(myDeck):
+        if myTable[i] != 'X':
+            for k in range (i+1 , len(myDeck)):
+                if myDeck[k][0] == myDeck[i][0] and myTable[i] != 'X':
+                    return True
+    return False
+
+def checkSimilarSymbols(playStyle, bonusPoints, myDeck, myTable):
+    for i in len(myDeck):
+        if myTable[i] != 'X':
+            for k in range (i+1 , len(myDeck)):
+                if myDeck[k][1] == myDeck[i][1] and myTable[i] != 'X':
+                    return True
+    return False
+#########################################################################
 
 #First things first, we need a deck.
 #Like  every programmer, we seek for the easiest way possible
@@ -6,12 +39,6 @@
 
 #########################################################################
 ################## D E C K   C R E A T I O N ############################
-import itertools
-import random
-import numpy as np
-import array
-import time
-import os
 
 symbols=['♥','♦','♣','♠']
 specials = ['10','J', 'Q', 'K']
@@ -31,7 +58,14 @@ Suits = ['spades', 'clubs', 'hearts', 'diamonds']
 easydeck = list(itertools.product(easyVals, Suits))
 normaldeck = list(itertools.product(normalVals, Suits))
 deck = list(itertools.product(Vals, Suits))
-worthings = list(itertools.product(Vals, scorings))
+worthings = []
+myc = 0
+for card in Vals2:
+    if card == 'A':
+        worthings.append([card,1])
+        break
+    worthings.append([card,scorings[myc]])
+    myc +=1
 
 '''
 random.shuffle(easydeck)
@@ -47,6 +81,26 @@ for val, suit in deck:
 
 #########################################################################
 ############### S E C O N D A R Y   F U N C T I O N S  ##################
+def electWinner(scoreBoard):
+    counter = 1
+    max = 0
+    maxi = 0
+    draw = False
+    drawi = 0
+    for score in scoreBoard:
+        if score > max:
+            max = score
+            maxi = counter
+            draw = False
+        elif score == max:
+            draw = True
+            drawi = counter
+    if not draw:
+        print("The big winner is Player " + str(maxi) + " !\n")
+    else:
+        print("The game is a draw between Player " + str(maxi) + " and Player " + str(drawi) + " !\n")
+    time.sleep(5)
+
 
 def gameDeck(playStyle):
     if playStyle == 1 :
@@ -88,38 +142,22 @@ def gameTable(playStyle):
         x,y = 4,13
     return ['X' for i in range(y*x)]
 
-def checkSimilarValues(playStyle, bonusPoints, myDeck, myTable):
-    for i in len(myDeck):
-        if myTable[i] != 'X':
-            for k in range (i+1 , len(myDeck)):
-                if myDeck[k][0] == myDeck[i][0] and myTable[i] != 'X':
-                    return True
-    return False
-
-def checkSimilarSymbols(playStyle, bonusPoints, myDeck, myTable):
-    for i in len(myDeck):
-        if myTable[i] != 'X':
-            for k in range (i+1 , len(myDeck)):
-                if myDeck[k][1] == myDeck[i][1] and myTable[i] != 'X':
-                    return True
-    return False
-
 def checkGameProgress(myTable):
     counter = 0
     for value in myTable:
-        if value = 'X':
-            counter ++
+        if value == 'X':
+            counter +=1
     if counter < 2 :
         return False
     else:
         return True
 
-def printCurrentOpportunities(myTable):
+def printCurrentOpportunities(playStyle, myTable):
     if playStyle == 1 :
         for i in range (0,4):
             print("\n")
             for j in range (0,4):
-                print('X', end = "\t", flush=True)
+                print(myTable[i * 4 + j ], end = "\t", flush=True)
     elif playStyle == 2:
         for i in range (0,4):
             print("\n")
@@ -130,55 +168,77 @@ def printCurrentOpportunities(myTable):
             print("\n")
             for j in range (0,13):
                 print('X', end = "\t", flush=True)
+    print("\n")
+
+def printscoreBoard(scoreBoard):
+    os.system("clear")
+    c = 1
+    for player in scoreBoard:
+        print("Player " + str(c) + ":  "  + str(player) + " Points")
+        c += 1
+    print("\n")
+    time.sleep(5)
+
 #########################################################################
 ##################### G A M E  F U N C T I O N S  #######################
-def checkPotentialPoints(playStyle, bonusPoints, myDeck, myTable):
-    if bonusPoints:
-        return checkSimilarValues(playStyle, bonusPoints, myDeck, myTable) and checkSimilarSymbols(playStyle, bonusPoints, myDeck, myTable)
-    else:
-        return checkSimilarValues(playStyle, bonusPoints, myDeck, myTable)
+
 
 def checkKings(s1, s2, myDeck,myTable):
     if myDeck[s1][0] in specials and myDeck[s2][0] in specials:
-        if myDeck[s1][0] == 'K' and myDeck[s2][0] = 'K':
-            print("You won another pick!")
-            return True
+        if myDeck[s1][0] == 'K' and myDeck[s2][0] == 'K':
+            if checkGameProgress(myTable):
+                return True
     return False
 
 def checkJacks(s1, s2, myDeck,myTable):
     if myDeck[s1][0] in specials and myDeck[s2][0] in specials:
-        if myDeck[s1][0] == 'J' and myDeck[s2][0] = 'J':
-            print("You won another round!")
-            return True
+        if checkGameProgress(myTable):
+            if myDeck[s1][0] == 'J' and myDeck[s2][0] == 'J':
+                print("You won another round!")
+                return True
     return False
 
 def checkThirdDrawPotential(sum1, sum2, myDeck, myTable):
-    if myDeck[s1][0] in specials and myDeck[s2][0] in specials:
-        if myDeck[s1][0] == 'Q' and myDeck[s2][0] = 'J':
-            print("You won another chance!")
-            return True
-        elif myDeck[s1][0] == 'J' and myDeck[s2][0] = 'Q':
-            print("You won another chance!")
-            return True
+    if myDeck[sum1][0] in specials and myDeck[sum2][0] in specials:
+        if checkGameProgress(myTable):
+            if myDeck[sum1][0] == 'Q' and myDeck[sum2][0] == 'J':
+                print("You won another pick!")
+                return True
+            elif myDeck[sum1][0] == 'J' and myDeck[sum2][0] == 'Q':
+                print("You won another pick!")
+                return True
     return False
 
-def checkPoints(sum1, sum2, sum3, myDeck, myTable):
+def checkPoints(sum1, sum2, sum3, myDeck, myTable, scoreBoard, currentPlayer, bonusPoints):
+    time.sleep(2)
     added1, added2, added3 = False, False, False
+    symbol1, symbol2 = '' , ''
     score = 0
     for matches in worthings:
         if matches[0] == myDeck[sum1][0] and not added1:
+            myTable[sum1] = myDeck[sum1][0] + "" + myDeck[sum1][1]
             score += matches[1]
             added1 = True
+            symbol1 = myDeck[sum1][1]
         if matches[0] == myDeck[sum2][0] and not added2:
+            myTable[sum2] = myDeck[sum2][0] + "" + myDeck[sum2][1]
             score += matches[1]
             added2 = True
-        if sum3 == -1: and matches[0] == myDeck[sum3][0] and not added3:
+            symbol2 = myDeck[sum2][1]
+        if sum3 != -1 and matches[0] == myDeck[sum3][0] and not added3:
+            myTable[sum3] = myDeck[sum3][0] + "" + myDeck[sum3][1]
             score += matches[1]
             added3 = True
-    return score
+    if bonusPoints and symbol1 == symbol2:
+        print("\n+10 points for same symbols!\n")
+        score += 10
+    print("Totally scored " + str(score) + " points on this round!\n")
+    scoreBoard[currentPlayer - 1] += score
+    time.sleep(4)
+    printscoreBoard(scoreBoard)
 
-def gameON(playStyle, bonusPoints, ai, players, myDeck, myTable):
-    currentPlayer = 0
+def gameON(playStyle, bonusPoints, ai, players, myDeck, myTable, scoreBoard):
+    currentPlayer = 1
     line, column = -1,-1
     mline, mcolumn = 4,0
     sum1,sum2,sum3 = 0,0,0
@@ -188,64 +248,70 @@ def gameON(playStyle, bonusPoints, ai, players, myDeck, myTable):
         mcolumn = 10
     elif playStyle == 3:
         mcolumn = 13
-    while checkGameProgress(playStyle, myTable):
+    while checkGameProgress(myTable):
         os.system("clear")
-        printCurrentOpportunities(myTable)
+        printCurrentOpportunities(playStyle, myTable)
+        if currentPlayer > players:
+            currentPlayer -= players
         while True:
             while True:
-              line = input("Player " + str(currentPlayer + 1) + " choose line number")
+              line = input("Player " + str(currentPlayer) + " choose line number\n")
               if int(line) > 0 and int(line) <= mline:
                   break
             while True:
-              column = input("Player " + str(currentPlayer + 1) + " choose column number")
+              column = input("Player " + str(currentPlayer) + " choose column number\n")
               if int(column) > 0 and int(column) <= mcolumn:
                   break
-            sum1 = line * 4 - 4 + column -1
+            sum1 = int(line) * 4 - 4 + int(column) -1
             if myTable[sum1] == 'X':
                 break
             else:
-                print("This card has already been flipped")
+                print("This card has already been flipped\n")
         print("You draw " + myDeck[sum1][0] + myDeck[sum1][1])
         while True:
             while True:
-              line = input("Player " + str(currentPlayer + 1) + " choose line number")
+              line = input("Player " + str(currentPlayer) + " choose line number\n")
               if int(line) > 0 and int(line) <= mline:
                   break
             while True:
-              column = input("Player " + str(currentPlayer + 1) + " choose column number")
+              column = input("Player " + str(currentPlayer) + " choose column number\n")
               if int(column) > 0 and int(column) <= mcolumn:
                   break
-            sum2 = line * 4 - 4 + column - 1
+            sum2 = int(line) * 4 - 4 + int(column) -1
             if myTable[sum2] == 'X':
                 break
             else:
-                print("This card has already been flipped")
+                print("This card has already been flipped\n")
         print("You draw " + myDeck[sum2][0] + myDeck[sum2][1])
         if checkKings(sum1, sum2, myDeck, myTable):
-            scorings[currentPlayer] += checkPoints(sum1, sum2, -1, myDeck, myTable)
+            print("Calculating score for player " + str(currentPlayer) + "...\n")
+            checkPoints(sum1, sum2, -1, myDeck, myTable, scoreBoard, currentPlayer, bonusPoints)
             currentPlayer += 2
         elif checkJacks(sum1, sum2, myDeck, myTable):
-            scorings[currentPlayer] += checkPoints(sum1, sum2, -1, myDeck, myTable)
+            print("Calculating score for player " + str(currentPlayer) + "...\n")
+            checkPoints(sum1, sum2, -1, myDeck, myTable, scoreBoard, currentPlayer, bonusPoints)
         elif checkThirdDrawPotential(sum1, sum2, myDeck, myTable):
             while True:
                 while True:
-                  line = input("Player " + str(currentPlayer + 1) + " choose line number")
+                  line = input("Player " + str(currentPlayer) + " choose line number\n")
                   if int(line) > 0 and int(line) <= mline:
                       break
                 while True:
-                  column = input("Player " + str(currentPlayer + 1) + " choose column number")
+                  column = input("Player " + str(currentPlayer) + " choose column number\n")
                   if int(column) > 0 and int(column) <= mcolumn:
                       break
-                sum3 = line * 4 - 4 + column -1
+                sum3 = int(line) * 4 - 4 + int(column) -1
                 if myTable[sum3] == 'X':
                     break
                 else:
-                    print("This card has already been flipped")
+                    print("This card has already been flipped\n")
             print("You draw " + myDeck[sum1][0] + myDeck[sum1][1])
-            scorings[currentPlayer] += checkPoints(sum1, sum2, sum3, myDeck, myTable)
+            print("Calculating score for player " + str(currentPlayer) + "...\n")
+            checkPoints(sum1, sum2, sum3, myDeck, myTable, scoreBoard, currentPlayer, bonusPoints)
             currentPlayer += 1
         else:
-            scorings[currentPlayer] += checkPoints(sum1, sum2, -1, myDeck, myTable)
+            print("Calculating score for player " + str(currentPlayer) + "...\n")
+            checkPoints(sum1, sum2, -1, myDeck, myTable, scoreBoard, currentPlayer, bonusPoints)
             currentPlayer += 1
     return False
 
@@ -254,14 +320,15 @@ def letsPlaySomeCards(playStyle, bonusPoints, ai, players):
     #first the difficulty allows us to create the game deck
     myDeck = gameDeck(playStyle)
     os.system("clear")
-    #Now lets make our table and our scoreboard
+    #Now lets make our table and our scoreBoard
     myTable = gameTable(playStyle)
-    scoreBoard = [0 for i in range(y)]
+    scoreBoard = [0 for i in range(int(players))]
     #and now we can start the game
     gaming = True
     while gaming:
-        gaming = gameON(playStyle, bonusPoints, ai, players, myDeck, myTable)
-    print("No potential points to be scored...\n GAME OVER!")
+        gaming = gameON(playStyle, bonusPoints, ai, players, myDeck, myTable, scoreBoard)
+    print("No potential points to be scored...\n GAME OVER!\n")
+    electWinner(scoreBoard)
     time.sleep(2)
     os.system("clear")
     print("That was fun! Another round maybe?")
@@ -288,8 +355,8 @@ deckPrint = 0
 players = 1
 tempAnswer = 'n'
 #lets declare our settings
-bonusPoints = 0
-ai = 0
+bonusPoints = False
+ai = False
 os.system("clear")
 print("Hello and welcome to this new trend... Please select mode!")
 while int(userInput) != 4:
@@ -297,15 +364,18 @@ while int(userInput) != 4:
     print("2 settings")
     print("3 learn your decks")
     print("4 exit")
-    while True:
-      userInput = input("Enter your selection... ")
-      if int(userInput) > 0 and int(userInput) < 5:
-          break
+    try:
+        while True:
+          userInput = input("Enter your selection... ")
+          if int(userInput) > 0 and int(userInput) < 5:
+              break
+    except ValueError as e:
+        pass
     if int(userInput) == 1:
         print("Please select a difficulty option...")
-        print("1.Easy: Begginer level deck (Max 2 players)")
-        print("2.Normal: Okay to play for practise (Max 3 players)")
-        print("3.Hard: How the game is meant to be played. Uses all the deck!(Max 4 players)")
+        print("1.Easy: Begginer level deck (2 players)")
+        print("2.Normal: Okay to play for practise (2-3 players)")
+        print("3.Hard: How the game is meant to be played. Uses all the deck!(2-4 players)")
         while True:
             playStyle = input("Enter your selection... ")
             if int(playStyle) > 0 and int(playStyle) < 4:
@@ -314,11 +384,11 @@ while int(userInput) != 4:
             players = input("Enter number of players... Take notice of the selected difficulty and settings!")
             if ai == 1 and int(players) == 1:
                 break
-            elif int(playStyle) == 1 and int(players) < 3 and int(players) > 0:
+            elif int(playStyle) == 1 and int(players) == 2:
                 break
-            elif int(playStyle) == 2 and int(players) < 4 and int(players) > 0:
+            elif int(playStyle) == 2 and int(players) < 4 and int(players) > 1:
                 break
-            elif int(playStyle) == 3 and int(players) < 5 and int(players) > 0:
+            elif int(playStyle) == 3 and int(players) < 5 and int(players) > 1:
                 break
         letsPlaySomeCards(int(playStyle), bonusPoints, ai, int(players))
     elif int(userInput) == 2:
@@ -326,19 +396,25 @@ while int(userInput) != 4:
         print("Please select a setting option to modify...")
         print("1.bonus bonus points on similar suit")
         print("2.ai oppoment (one player only)")
-        while True:
-          userSetting = input("Enter your selection... ")
-          if int(userSetting) > 0 and int(userSetting) < 4:
-              break
-        while True:
-          tempAnswer = input("Are you sure you want to enable this option? y/n")
-          if tempAnswer == 'y' or  tempAnswer == 'n':
-              break
+        try:
+            while True:
+              userSetting = input("Enter your selection... ")
+              if int(userSetting) > 0 and int(userSetting) < 4:
+                  break
+        except ValueError as e:
+            pass
+        try:
+            while True:
+              tempAnswer = input("Are you sure you want to enable this option? y/n")
+              if tempAnswer == 'y' or  tempAnswer == 'n':
+                  break
+        except ValueError as e:
+            pass
         if tempAnswer == 'y':
             if userSetting == 1:
-                bonusPoints = 1
+                bonusPoints = True
             else:
-                ai = 1
+                ai = True
         userSetting = 0
     elif int(userInput) == 3:
         userInput = 0
@@ -346,10 +422,14 @@ while int(userInput) != 4:
         print("1.Easy: Begginer level deck")
         print("2.Normal: Okay to play for practise")
         print("3.Hard: How the game is meant to be played. Uses all the deck!")
-        while True:
-          deckPrint = input("Enter your selection... ")
-          if int(deckPrint) > 0 and int(deckPrint) < 4:
-              break
+        print("4.See whay each card worths!")
+        try:
+            while True:
+              deckPrint = input("Enter your selection... ")
+              if int(deckPrint) > 0 and int(deckPrint) < 5:
+                  break
+        except ValueError as e:
+            pass
         if int(deckPrint) == 1:
             for val, suit in easydeck:
                 print('The %s of %s' % (val, suit))
@@ -359,6 +439,9 @@ while int(userInput) != 4:
         elif int(deckPrint) == 3:
             for val, suit in deck:
                 print('The %s of %s' % (val, suit))
+        elif int(deckPrint) == 4:
+            for val, suit in worthings:
+                print('The %s scores %s' % (val, suit))
     elif int(userInput) == 4:
         print("We know you'll come back soon <3")
 #########################################################################
